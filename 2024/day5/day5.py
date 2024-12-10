d26ec3b5c2be424442d5553eae2b1b8847337fc5
River McCubbin <mccubbinri@gmail.com>
2024-12-09 20:56:38 -0500

    completed part 1

--- 2024/day5/day5.py
+++ 2024/day5/day5.py
@@ -1,27 +1,40 @@
-
-
-def get_ordering(datafile="sample.txt"):
-    ordering = {}
-    
+def p1(datafile="sample.txt"):
     with open(datafile, 'r') as data:
+        order = {}
+
         line = data.readline()
+
         while line != '\n':
-            left, right = line.split('|')
-            if left not in ordering:
-                ordering[left] = []
-            ordering[left].append(right)
-            data.readline()
+            first, last = map(int, line.split('|'))
+            if first not in order:
+                order[first] = []
+            order[first].append(last)
+            line = data.readline()
 
-    return ordering
+        print(order)
 
-def get_updates(datafile="sample.txt"):
-    updates = []
+        total = 0
+    
+        for line in data:
+            nums = [int(num) for num in line.strip().split(',')]
+            if is_ordered(nums, order):
+                print(f"{nums} is in order")
+                print(f"\tadding {nums[len(nums) // 2]}")
+                total += int(nums[len(nums) // 2])
+            else:
+                print(f"{nums} is not in order")
 
-    with open(datafile, 'r') as data:
-        updates = data.readlines()
-
-    return updates
+    return total
 
 
-def is_in_order(update, ordering):
-    return update
+def is_ordered(nums, order):
+    for i in range(len(nums)):
+        for num in nums[:i]:
+            if nums[i] in order and num in order[nums[i]]:
+                return False
+
+    return True
+
+
+if __name__ == "__main__":
+    print(p1(datafile="input.txt"))
