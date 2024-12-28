7ae241f0d674ce20954a5a7767103dab2c2571fc
River McCubbin <mccubbinri@gmail.com>
2024-12-27 22:53:19 -0500

    no longer going to try rust

--- 2024/day5/day5.py
+++ 2024/day5/day5.py
@@ -1,15 +1,6 @@
 def p1(datafile="sample.txt"):
     with open(datafile, 'r') as data:
-        order = {}
-
-        line = data.readline()
-
-        while line != '\n':
-            first, last = map(int, line.split('|'))
-            if first not in order:
-                order[first] = []
-            order[first].append(last)
-            line = data.readline()
+        order = get_order(data)
 
         print(order)
 
@@ -18,13 +9,27 @@ def p1(datafile="sample.txt"):
         for line in data:
             nums = [int(num) for num in line.strip().split(',')]
             if is_ordered(nums, order):
-                print(f"{nums} is in order")
-                print(f"\tadding {nums[len(nums) // 2]}")
                 total += int(nums[len(nums) // 2])
-            else:
-                print(f"{nums} is not in order")
 
     return total
+
+
+def get_order(data):
+    """
+    return a dictionary with keys that must occur before any of their values
+
+    data: open file with order data
+    """
+    order = {}
+    line = data.readline()
+    while line != '\n':
+        first, last = map(int, line.split('|'))
+        if first not in order:
+            order[first] = []
+        order[first].append(last)
+        line = data.readline()
+
+    return order
 
 
 def is_ordered(nums, order):
@@ -36,5 +41,34 @@ def is_ordered(nums, order):
     return True
 
 
+def p2(datafile="sample.txt"):
+    with open(datafile, 'r') as data:
+        order = get_order(data)
+
+        total = 0
+
+        for line in data:
+            nums = [int(num) for num in line.strip().split(',')]
+            if not is_ordered(nums, order):
+                correct_nums = correct_order(nums, order)
+                print(f"ordered {nums} as {correct_nums}")
+                val = int(correct_nums[len(correct_nums) // 2])
+                print(f"adding {val}")
+                total += val
+    
+    return total
+
+
+def correct_order(given_nums, order):
+    nums = [num for num in given_nums]
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            if nums[i] in order and nums[j] in order[nums[i]]:
+                nums[i], nums[j] = nums[j], nums[i]
+
+    return nums
+    
+
 if __name__ == "__main__":
-    print(p1(datafile="input.txt"))
+    print(p2(datafile="input.txt"))
+    #print(p1(datafile="input.txt"))
